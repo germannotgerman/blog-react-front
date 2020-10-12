6b58b9acf3fd291a069ed535250e59b980e3a33f
German Mumma <german.mumma@abcgrupp.ee>
2020-10-12 14:20:43 +0300

    Add working front-end with dummy data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+import NavBar from "./NavBar";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <NavBar/>
+                <div id="page-body">
+                    <Switch>
+                        <Route path="/" component={HomePage} exact/>
+                        <Route path="/about" component={AboutPage}/>
+                        <Route path="/articles-list" component={ArticlesListPage}/>
+                        <Route path="/article/:name" component={ArticlePage}/>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+
+    );
 }
 
 export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+        <h1>Hello, welcome to my blog</h1>
+        <p>
+            Egyptian mau savannah malkin or cornish rex.
+            Singapura devonshire rex. Norwegian forest lynx so kitten. Tomcat kitty yet tiger and
+            russian blue munchkin, ragdoll. Devonshire rex. Norwegian forest. Tomcat lynx yet cougar tiger or
+            siberian. Singapura sphynx for bombay. Panther bengal. Egyptian mau balinese . Mouser balinese american
+            shorthair. Mouser norwegian forest. Cornish rex kitten tabby malkin. British shorthair cheetah. Bengal
+            himalayan so grimalkin. Cougar balinese for british shorthair, birman so siberian donskoy savannah. Donskoy
+            scottish fold singapura for tabby. Havana brown kitten kitty balinese and persian.
+        </p>
+        <p>
+            Mouser balinese american
+            shorthair. Mouser norwegian forest. Cornish rex kitten tabby malkin. British shorthair cheetah. Bengal
+            himalayan so grimalkin. Cougar balinese for british shorthair, birman so siberian donskoy savannah. Donskoy
+            scottish fold singapura for tabby. Havana brown kitten kitty balinese and persian.
+        </p>
+    </>
+
+);
+
+export default HomePage;
